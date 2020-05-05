c846fe1ca887d6221fd12092a1f476a0f82e5209
VictoriaaSong <60018283+VictoriaaSong@users.noreply.github.com>
2020-05-05 18:38:01 -0400

    Update ast.py
    printstmt, block

--- ast.py
+++ ast.py
@@ -127,6 +127,11 @@ class Block(Stmt):
                 temp = temp + str(j)
         return temp
 
+    def eval(self, env):
+        # TODO condition for empty list?
+        for i in self.stmts:
+            i.eval(env)
+
 
 class IfStmt(Stmt):
     def __init__(self, cond: Expr, truepart: Stmt, falsepart: [Stmt]):
@@ -173,6 +178,11 @@ class PrintStmt(Stmt):
                 pri = pri + ", " + str(i)
         pri  = pri + ")"
         return pri
+       
+   def eval(self, env):
+        self.pArg.eval(env)
+        if self.pArgList is not None:
+            self.pArgList.eval(env)
 
 
 class ReturnStmt(Stmt):
